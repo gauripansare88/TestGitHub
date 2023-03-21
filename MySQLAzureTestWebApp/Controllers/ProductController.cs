55c5295edb7b4966a3157340c1b8a52871a7120b
Gauri Pansare <gauripansare88@gmail.com>
2023-03-21 16:56:46 +0530

    commit for the first time

---
+++ MySQLAzureTestWebApp/Controllers/ProductController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySQLAzureTestWebApp.Models;
+using MySQLAzureTestWebApp.Services;
+
+namespace MySQLAzureTestWebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Produc
+        public ActionResult Index()
+        {
+            ProductService PS = new ProductService();
+            List<Product> products = PS.GetProducts();
+            return View(products);
+        }
+
+        // GET: Produc/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Produc/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Produc/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Produc/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Produc/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Produc/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Produc/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MySQLAzureTestWebApp/Services/ProductService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MySQLAzureTestWebApp.Models;
+
+namespace MySQLAzureTestWebApp.Services
+{
+    public class ProductService
+    {
+        private static string db_source = "mytestdbsql.database.windows.net";
+        private static string db_user = "demouser";
+        private static string db_password = "learningdevops@123";
+        private static string db_database= "mytestdb";
+        
+
+        private SqlConnection GetConnection()
+        {
+            var _builder = new SqlConnectionStringBuilder();
+            _builder.DataSource = db_source;
+            _builder.UserID = db_user;
+            _builder.Password = db_password;
+            _builder.InitialCatalog = db_database;
+            return new SqlConnection(_builder.ConnectionString);
+        }
+        public List<Product> GetProducts()
+        {
+            SqlConnection con = GetConnection();
+            List<Product> _product_lst = new List<Product>();
+            string statement = "select ProductID,ProductName,Quantity from Products";
+            con.Open();
+            SqlCommand cmd = new SqlCommand(statement,con);
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Product product = new Product()
+                    {
+                        ProductId = reader.GetInt32(0),
+                        ProductName = reader.GetString(1),
+                        Quantity = reader.GetInt32(2)
+                    };
+                    _product_lst.Add(product);
+
+                }
+            } 
+            con.Close();
+
+            return _product_lst;
+        } 
+    }
+}
